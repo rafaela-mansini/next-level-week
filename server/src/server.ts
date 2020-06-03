e3fdb5d62ef11b5c48ce9311e9a6e648b2a1e0f1
rafaela_mansini <rafaela_mansini@hotmail.com>
2020-06-03 20:14:38 -0300

    Learning about routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,47 @@
-import express from 'express';
+import express, { request } from 'express';
 
 const app = express(); //criar a constante do tipo express
+app.use(express.json()); //inserir uma funcionalidade falando que o express entende o corpo da requisição em formato json
+
+const users = ['Rafaela', 'Ederson', 'Chewie', 'Leia', 'Han', 'Luke'];
 
 //função get utiliza dois parametros, a url e a função que vai realizar o que deseja
 app.get('/users', (request, response) =>{
     //request obtem dados da requisição | response devolver resposta para a aplicação
-    console.log('Listagem de usuários');
-
     //response.send('Hello World'); //enviar resposta simples (string) para aplicação
-    response.json([
-        'Rafaela', 'Ederson', 'Chewie', 'Leia', 'Han', 'Luke'
-    ]); //retorna json para aplicação
+
+    const search = String(request.query.search); //query que vem do request que pode ou não ser opcional
+    //filter recebe um parametro a ser utilizado como item unico | includes verifica se o parametro passado (search) está icluido dentro da variavel verificada (user)
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users; 
+
+    return response.json(filteredUsers); //retorna json para aplicação
+});
+
+app.get('/users/:id', (request, response) => { //busca apenas um usuário na requisição
+    const id = Number(request.params.id); //por utilizar o typescript é necessário informar que é um number, pois os requests vem como parametro strings
+    const user = users[id];
+    
+    return response.json(user);
+});
+
+app.post('/users', (request, response) => { 
+    const data = request.body;
+
+    const user = {
+        name: data.name,
+        email: data.email
+    };
+
+    console.log(user);
+    
+    return response.json(user);
 });
 
 //porta a qual o servidor vai rodar, pode ser inserido qualquer porta...
 app.listen(3333);
+
+/*Additional documentation 
+*   Request Parm: Parametros da rota que identificam um recurso (parametro obrigatório);
+*   Query Param: Parametros enviados para a rota --informações do request que vem depois de ?params(parametros opicionais);
+*   Request body: Parametros que vem com o corpo da requisição
+*/

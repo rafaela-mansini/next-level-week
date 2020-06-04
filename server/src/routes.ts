a0e74311ab8b44bfaf4ea6dd9d643cdfcf5151aa
rafaela_mansini <rafaela_mansini@hotmail.com>
2020-06-03 22:20:17 -0300

    Items list

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router(); //desacoplar rotas do arquivo principal do servidor
 
-routes.get('/', (request, response) =>{
-    return response.json({ message: 'Hello World'});
+routes.get('/items', async (request, response) =>{
+    const items = await knex('items').select('*'); //SELECT * FROM items -- como a requisição pode demorar, utiliza-se o async/await na função
+
+    const serializedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+        }
+    });
+    return response.json(serializedItems);
 }); 
 
 export default routes;

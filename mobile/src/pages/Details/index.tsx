54ca077edd24b435e1de00bdbed075f69d3b3bf7
rafaela_mansini <rafaela_mansini@hotmail.com>
2020-06-07 14:29:17 -0300

    Detail screen layout

--- mobile/src/pages/Details/index.tsx
+++ mobile/src/pages/Details/index.tsx
@@ -1,9 +1,119 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
+import { View, StyleSheet, Text, TouchableOpacity, Image, SafeAreaView } from 'react-native';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 const Details = () => {
-    return (<View><Text>Details</Text></View>); 
-}
+    const navigation = useNavigation();
+
+    function hanldeNavigateBack(){
+        navigation.goBack();
+    }
+    return (
+        <SafeAreaView style={{flex: 1}}>
+            <View style={styles.container}>
+                <TouchableOpacity onPress={hanldeNavigateBack}>
+                    <Icon name="arrow-left" size={20} color="#34cb79" />
+                </TouchableOpacity>
+
+                <Image style={styles.pointImage} source={{ uri: "http://192.168.0.7:3333/uploads/default.jpg" }} />
+
+                <Text style={ styles.pointName }>Mercado do Totó</Text>
+                <Text style={ styles.pointItems }>Lâmpada, Óleo de cozinha</Text>
+
+                <View style={styles.address}>
+                    <Text style={styles.addressTitle}>Endereço</Text>
+                    <Text style={styles.addressContent}>Mogi das Cruzes, SP</Text>
+                </View>
+            </View>
+
+            <View style={styles.footer}>
+                <RectButton style={styles.button} onPress={() => {} }>
+                    <FontAwesome name="whatsapp" size={20} color="#FFF" />
+                    <Text style={styles.buttonText}>Whatsapp</Text>
+                </RectButton>
+                <RectButton style={styles.button} onPress={() => {} }>
+                    <Icon name="mail" size={20} color="#FFF" />
+                    <Text style={styles.buttonText}>E-mail</Text>
+                </RectButton>
+            </View>
+        </SafeAreaView>
+    ); 
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 32,
+      paddingTop: 20,
+    },
+  
+    pointImage: {
+      width: '100%',
+      height: 120,
+      resizeMode: 'cover',
+      borderRadius: 10,
+      marginTop: 32,
+    },
+  
+    pointName: {
+      color: '#322153',
+      fontSize: 28,
+      fontFamily: 'Ubuntu-Bold',
+      marginTop: 24,
+    },
+  
+    pointItems: {
+      fontFamily: 'Roboto-Regular',
+      fontSize: 16,
+      lineHeight: 24,
+      marginTop: 8,
+      color: '#6C6C80'
+    },
+  
+    address: {
+      marginTop: 32,
+    },
+    
+    addressTitle: {
+      color: '#322153',
+      fontFamily: 'Roboto-Bold',
+      fontSize: 16,
+    },
+  
+    addressContent: {
+      fontFamily: 'Roboto-Regular',
+      lineHeight: 24,
+      marginTop: 8,
+      color: '#6C6C80'
+    },
+  
+    footer: {
+      borderTopWidth: StyleSheet.hairlineWidth,
+      borderColor: '#999',
+      paddingVertical: 20,
+      paddingHorizontal: 32,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    
+    button: {
+      width: '48%',
+      backgroundColor: '#34CB79',
+      borderRadius: 10,
+      height: 50,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+  
+    buttonText: {
+      marginLeft: 8,
+      color: '#FFF',
+      fontSize: 16,
+      fontFamily: 'Roboto-Bold',
+    },
+});
 
 export default Details;

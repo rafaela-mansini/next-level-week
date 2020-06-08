fa604b504611a410236f971ae39bc7b87f040d35
rafaela_mansini <rafaela_mansini@hotmail.com>
2020-06-07 21:18:05 -0300

    Validate data with celebrate -- Joi

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { celebrate, Joi } from 'celebrate';
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 import multer from 'multer';
@@ -15,6 +16,25 @@ routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
 
-routes.post('/points', upload.single('image'), pointsController.create); // passando o upload para o campo image (que é o campo que estou usando agora)
+routes.post(
+    '/points',
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required(), //regex ver como faz para validação de virgula
+            // validar imagem também com filter
+        })
+    }, {
+        abortEarly: false
+    }),
+    pointsController.create
+); // passando o upload para o campo image (que é o campo que estou usando agora)
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express, { request } from 'express';
 import path from 'path';
 import cors from 'cors';
 import routes from './routes';
+import { errors } from 'celebrate';
 
 const app = express(); //criar a constante do tipo express
 
@@ -9,6 +10,8 @@ app.use(cors()); //informar qual url das aplicações podem acessar o sistema
 app.use(express.json()); //inserir uma funcionalidade falando que o express entende o corpo da requisição em formato json
 app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads'))); // cria rota para acessar arquivos estáticos no sistema
+
+app.use(errors()); // forma que lida com os erros no front-end
 
 //porta a qual o servidor vai rodar, pode ser inserido qualquer porta...
 app.listen(3333);
